351e658766bb6d97eb3bf8c434acdf95983d7d02
andy64lol <andy64lolxd@gmail.com>
2026-03-14 10:26:28 +0100

    Auto-commit: 1 changes (1 modified, 0 added, 0 deleted) in /home/andy64lolxd/our_legacy_2 [from gitauto]

--- app.py
+++ app.py
@@ -3403,7 +3403,5 @@ def new_game():
     return redirect(url_for("index"))
 
 port = int(os.environ.get("PORT", 5000))
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=False)
