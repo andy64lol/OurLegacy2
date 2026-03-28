c2a215a62a38d6842599043feace5906f079a2da
andy64lol <andy64lolxd@gmail.com>
2026-03-28 14:47:04 +0100

    Auto-commit: 2 changes (2 modified, 0 added, 0 deleted) in /home/andy64lolxd/our_legacy_2 [from gitauto]

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from gevent import monkey
+
+monkey.patch_all()
+
+import sys
+
 """
 Our Legacy 2 - Flask Web Interface
 Medieval fantasy RPG playable in the browser.
--- init.sh
+++ init.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e
-
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-fi
-
-source venv/bin/activate
-
-pip install -r requirements.txt
-python3 app.py
+gunicorn app:app --bind 0.0.0.0:$PORT --worker-class gevent
