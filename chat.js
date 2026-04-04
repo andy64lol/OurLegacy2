ec103534878d6c4f162ccda43c6060da3c239256
Andy64lolxd <andy64lolxd@gmail.com>
2026-04-04 09:21:18 +0000

    Troll?

--- chat.js
+++ chat.js
@@ -1,2 +1,6 @@
-console.log("J'aime les chats.")
+console.log("J'aime les chats.");
 // Oui c'est vrai j'aime les chats
+// Je suis un commentaire
+// Je suis un autre commentaire
+// Je suis un commentaire en ligne
+// https://www.youtube.com/watch?v=dQw4w9WgXcQ
